4d0330505154a6e92f2357b9093a09f5b5012f4a
Om Shivhare <omshivhare666@gmail.com>
2026-07-06 23:35:26 +0530

    completed setup for tailwindcss

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,13 +2,13 @@
 import './App.css'
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-green-500">
+        WhatsApp Clone 🚀
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
